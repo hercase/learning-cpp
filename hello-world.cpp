58a1d9b4999d8d4667c61508eb0a7093ce00eed7
Hernan Case <her.case@live.com.ar>
2020-05-15 19:20:13 -0300

    TP2

---
+++ hello-world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    
+    cout << "Hola mi nombre es " << nombre << endl;
+    return 0;
+}
---
+++ tp2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "Unidad 2" << endl;
+    cout << "Ejercicio 1." << endl;
+    cout << "a. La suma de 45 + 34 es " << 45 + 34 << endl;
+    cout << "b. La resta de 45 - 40 es " << 45 - 40 << endl;
+    cout << "c. La division de 46 y 3 es " << 46 / 3 << endl;
+    cout << "" << endl;
+    cin.ignore();
+    // clrscr
+    cout << "\033[2J\033[1;1H";
+    cout << "Unidad 2" << endl;
+    cout << "Ejercicio 2." << endl;
+    float largo, ancho, alto;
+    cout << "a. Calcular un volumen de una habitacion." << endl;
+    cout << "Ingrese el largo : ";
+    cin >> largo;
+    cout << "Ingrese el ancho : ";
+    cin >> ancho;
+    cout << "Ingrese el alto : ";
+    cin >> alto;
+    cout << "El volumen calculado es: " << largo * ancho * alto << endl;
+    cout << "" << endl;
+    double pi, radio_base, altura, resultado;
+    cout << "b. Calcular el volumen de un cono" << endl;
+    cout << "Ingrese el radio de la base : ";
+    cin >> radio_base;
+    cout << "Ingrese la altura : ";
+    cin >> altura;
+    resultado = ( (pi * (radio_base * radio_base)) / altura ) / 3;
+    cout << "El volumen calculado es: " << resultado << endl;
+    pi = 3.14;
+    return 0;
+}
