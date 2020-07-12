5ef038878896d5da69cd8304d723af5136bd75c4
Hernan Case <her.case@live.com.ar>
2020-07-11 23:50:22 -0300

    Added ( Codigo ASCII )

--- extras.cpp
+++ extras.cpp
@@ -7,25 +7,32 @@
 
 using namespace std;
 
-void ejemploTabla();
-void ejemploSwitch();
+void multiplicationTable();
+void rangeSwitch();
+void codigoAscii();
 
 int main(){
   int op;
-  cout  << "¿Que ejemplo quiere ver?"
+  system("cls");
+  cout  << "Que ejemplo quiere ver?"
         << "\n1. Tabla de multiplicacion"
-        << "\n2. Switch con rango"
+        << "\n2. Switch con rango (dias)"
+        << "\n3. Encontrar codigo ASCII"
         << "\nIngrese una opcion: ";
         cin >> op;
-
+        system("cls");
         switch (op)
         {
         case 1:
-          ejemploTabla();
+          multiplicationTable();
           break;
 
         case 2:
-          ejemploSwitch();
+          rangeSwitch();
+          break;
+
+        case 3:
+          codigoAscii();
           break;
         }
   return 0;
@@ -33,7 +40,7 @@ int main(){
 
 
 // Tabla de multiplicacion
-void ejemploTabla(){
+void multiplicationTable(){
     for (int i = 1; i <= 10; i++){
     for (int j = 1; j <= 10; j++){
       cout << i << " x " << j << " = " << i * j << endl;
@@ -42,27 +49,43 @@ void ejemploTabla(){
 }
 
 // Switch con rangos.
-void ejemploSwitch(){
-  typedef enum { lunes, martes, miercoles, jueves, viernes, sabado, domingo } tDia;
-  
-  tDia dia = sabado;
+void rangeSwitch(){
+  int op;
 
-  switch (dia + 1){
+  cout  << "Que dia es hoy?"
+        << "\n1. Lunes"
+        << "\n2. Martes"
+        << "\n3. Miercoles"
+        << "\n4. Jueves"
+        << "\n5. Viernes"
+        << "\n6. Sabado"
+        << "\n7. Domingo"
+        << "\n Elija una dia: ";
+        cin >> op;
+
+  switch (op){
     case 1:
     case 2:
     case 3:
     case 4:
     case 5:
-      cout << "No es fin de semana! :( ";
+      cout << "\nNo es fin de semana! :( ";
       break;
 
     case 6:
     case 7:
-      cout << "Es fin de semana! :) ";
+      cout << "\nEs fin de semana! :) ";
       break;
 
     default:
-      cout << "¿Que dia es ese?";
+      cout << "\nQue dia es ese? :@";
       break;
   }
+}
+
+void codigoAscii(){
+  char c;
+  cout << "Ingrese un caracter: ";
+  cin >> c;
+  cout << "El codigo ASCII de " << c << " es " << int(c);
 }
